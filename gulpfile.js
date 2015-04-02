f28be93a620995f9bd19d5cddd917b140664e6c8
aetos <aetos@aerie.jp>
2015-04-02 16:36:17 +0900

    indent

--- gulpfile.js
+++ gulpfile.js
@@ -15,35 +15,35 @@ gulp.task("compile", function (callback) {
     
     fs.readFile("template.js", { encoding: "utf-8" }, function (error, data) {
     
-	    if (error) {
-	    	callback(error);
-	    	return;
-	    }
-	    
-	    var ts = gulp
-	    	.src("jQuery.SyntaxHighlighter.ts")
-	    	.pipe(sourcemaps.init())
-	        .pipe(tsc({
-	            declarationFiles: true,
-	            	sourceRoot: ".."
-	        }));
-	    
-	    var js = ts.js
-	        .pipe(wrap(data))
-	        .pipe(gulp.dest("dist"))
-	        .pipe(uglify())
-	        .pipe(rename("jQuery.SyntaxHighlighter.min.js"))
-	    	.pipe(sourcemaps.write("."))
-	        .pipe(gulp.dest("dist"));
-	        
-	    var dts = ts.dts
-	        .pipe(gulp.dest("dist"));
-	    
-	    merge([js, dts]);
-	    callback();
-	    
-	});
-	
+        if (error) {
+            callback(error);
+            return;
+        }
+        
+        var ts = gulp
+            .src("jQuery.SyntaxHighlighter.ts")
+            .pipe(sourcemaps.init())
+            .pipe(tsc({
+                declarationFiles: true,
+                sourceRoot: ".."
+            }));
+        
+        var js = ts.js
+            .pipe(wrap(data))
+            .pipe(gulp.dest("dist"))
+            .pipe(uglify())
+            .pipe(rename("jQuery.SyntaxHighlighter.min.js"))
+            .pipe(sourcemaps.write("."))
+            .pipe(gulp.dest("dist"));
+            
+        var dts = ts.dts
+            .pipe(gulp.dest("dist"));
+        
+        merge([js, dts]);
+        callback();
+        
+    });
+    
 });
 
 gulp.task("browserify", ["compile"], function () {
